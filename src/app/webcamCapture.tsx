73a49321f354271e7c434acad88ea0233f2cc244
Amya Moore <bookworm7572@gmail.com>
2024-11-19 16:28:59 -0700

    init commit

--- src/app/webcamCapture.tsx
+++ src/app/webcamCapture.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
-import imageReader from "./imageReader";
-import { redirect } from "next/dist/server/api-utils";
 
 export default function WebcamCapture() {
 	const webcamRef = useRef(null);
@@ -10,45 +8,16 @@ export default function WebcamCapture() {
 	const [imgSrc, setImgSrc] = useState(null);
 
 	const capture = useCallback(() => {
-		const imageSrc = webcamRef.current.getScreenshot();
-		setImgSrc(imageSrc);
-		// hello("https://ocr.space/Content/Images/receipt-ocr-original.webp");
-		hello(imageSrc);
+		if (webcamRef.current !== null) {
+			const imageSrc = webcamRef.current.getScreenshot();
+			setImgSrc(imageSrc);
+		}
+		setImgSrc(null);
 	}, [webcamRef]);
 
 	const retake = () => {
 		setImgSrc(null);
 	};
-
-	const [result, setResult] = useState<string | null>(null);
-
-	async function hello(image: string) {
-		if (image != null) {
-			try {
-				var myHeaders = new Headers();
-				myHeaders.append("apikey", "K89600633588957");
-
-				var formdata = new FormData();
-				formdata.append("base64Image", image);
-				formdata.append("language", "eng");
-				formdata.append("isOverlayRequired", "false");
-				formdata.append("detectOrientation", "true");
-				formdata.append("isTable", "false");
-
-				var requestOptions = {
-					method: "POST",
-					headers: myHeaders,
-					body: formdata,
-					redirect: "follow",
-				};
-
-				fetch("https://api.ocr.space/parse/image", requestOptions)
-					.then((response) => response.text())
-					.then((result) => setResult(result))
-					.then((error) => console.log("error", error));
-			} catch (error) {}
-		}
-	}
 
 	return (
 		<div className="container">
@@ -65,7 +34,6 @@ export default function WebcamCapture() {
 					<div>
 						<img src={imgSrc} alt="webcam" />
 						<p>{imgSrc}</p>
-						<p>{result ? result: 'none'}</p>
 					</div>
 				) : (
 					<p>no image</p>
